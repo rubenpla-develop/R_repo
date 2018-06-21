d935d674f10e72cbb6d12b81163b6ce7f322f3d6
Ruben Pla <rubenpla.develop@gmail.com>
2018-06-21 18:01:08 +0200

    [SCRIPT] New instruction added         \n- new comments

--- Hello_world_in_R.R
+++ Hello_world_in_R.R
@@ -1,6 +1,12 @@
 library(hflights)
 library(data.table)
 
+##DT[i, j, by]
+##  |
+##  |_____ i -> WHERE  
+##  |_____ j -> SELECT 
+##  |_____ by -> GROUPBY
+
 DT <- as.data.table(hflights)
 
 DT[Month==10,mean(na.omit(AirTime)), by=UniqueCarrier]
@@ -10,3 +16,9 @@ DT[.N-2]
 
 ## we avoid null values on 'ArrDelay' table field
 DT[, mean(na.omit(ArrDelay))]
+
+## when selecting several colimns to do some stuff in 'j' part, you need to use .() notation, equals
+## to 'list()'.
+## It RETURNS a DATA TABLE instead of a vector
+DT[, .(mean(na.omit(ArrDelay)), mean(na.omit(ArrDelay)))]
+
