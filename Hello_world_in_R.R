580d3e08e3d2dbfb17afa4c97d16a7cac4d8d009
Ruben Pla <rubenpla.develop@gmail.com>
2018-06-21 18:30:32 +0200

    [EXERCISE/SCRIPT] - Created exercise [7] predicate

--- Hello_world_in_R.R
+++ Hello_world_in_R.R
@@ -7,18 +7,25 @@ library(data.table)
 ##  |_____ j -> SELECT 
 ##  |_____ by -> GROUPBY
 
+## [1] create data table from hflights and making request with 'where', 'select' & 'groupBy' params
 DT <- as.data.table(hflights)
 
 DT[Month==10,mean(na.omit(AirTime)), by=UniqueCarrier]
 
-## '.N' it's a sign equal to length(DT) result 
+## [2] '.N' it's a sign equal to length(DT) result 
 DT[.N-2]
 
-## we avoid null values on 'ArrDelay' table field
+## [3] we avoid null values on 'ArrDelay' table field
 DT[, mean(na.omit(ArrDelay))]
 
-## when selecting several colimns to do some stuff in 'j' part, you need to use .() notation, equals
+## [4] when selecting several colimns to do some stuff in 'j' part, you need to use .() notation, equals
 ## to 'list()'.
 ## It RETURNS a DATA TABLE instead of a vector
-DT[, .(mean(na.omit(ArrDelay)), mean(na.omit(ArrDelay)))]
+DT[, .(mean(na.omit(DepDelay)), mean(na.omit(ArrDelay)))]
 
+## [5] Another useful feature which requires the ‘.()’ notation allows you to rename columns inside
+## the DT[…] command.
+DT[, .(Avg_ArrDelay = mean(na.omit(ArrDelay)))]
+
+## [6] We'll do the same as above in [4] but renaming result fields
+DT[, .(Avg_DepDelay = mean(na.omit(DepDelay)), Avg_ArrDelay = mean(na.omit(ArrDelay)))]
