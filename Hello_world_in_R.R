86c3977cb3c4bc9f59cc26c242aca00c4289f04f
Ruben Pla <rubenpla.develop@gmail.com>
2018-07-05 18:24:59 +0200

    [HELP]  - Added some comment lines about entities annotations [EXERCISES] Added [8] & [9] exercises

--- Hello_world_in_R.R
+++ Hello_world_in_R.R
@@ -7,6 +7,18 @@ library(data.table)
 ##  |_____ j -> SELECT 
 ##  |_____ by -> GROUPBY
 
+
+##NOTE about html entities 
+## SourceS : https://stackoverflow.com/questions/5068951/what-do-lt-and-gt-stand-for
+##          http://www.w3.org/TR/html4/sgml/entities.html
+##          http://www.w3schools.com/HTML/html_entities.asp
+##
+# &lt; stands for the less-than sign ( < )
+# &gt; stands for the greater-than sign ( > )
+# &le; stands for the less-than or equals sign ( ≤ )
+# &ge; stands for the greater-than or equals sign ( ≥ )
+
+
 ## [1] create data table from hflights and making request with 'where', 'select' & 'groupBy' params
 DT <- as.data.table(hflights)
 
@@ -38,3 +50,15 @@ DT[, .(Carriers = unique(na.omit(UniqueCarrier)))]
 
 ## [7] We'll try to show Average Dep & Arrive delays for every Carrier
 ## Aqui realizaras el ejercicio que hicimos el otro dia
+
+## [8] This exercises focuses on the 'by' part. THe 'by' part is used when we want to calculte the 'j' 
+## part grouped by specefic variable(or a manipulation). The 'j' expression is repeated for each 'by' group
+DT[, mean(na.omit(DepDelay)), by =Origin]
+
+## [9] Just as with the part 'j', you can do a lot of stuff in the 'by' part. Functions can be used in the
+## 'by' part  so that results if the operations  done in teh 'j' part are grouped by some condition specified
+## NOTE : the '.()' notation needs to be used when using several columns in the 'by' part
+DT[, .(Avg_DepDelay_byWeekDays = mean(na.omit(DepDelay))), by = .(Origin, Weekdays = DayOfWeek < 6)]
+
+
+
